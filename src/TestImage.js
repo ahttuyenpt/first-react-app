5b6f9dd0083487c2dd9d41653cecbb9ccc414181
Bryan Nguyen <bryan@mission.plus>
2022-03-22 16:38:57 +0700

    Homework

---
+++ src/TestImage.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+function TestImage (props) {
+
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+      fetch("https://jsonplaceholder.typicode.com/photos?_start=20&_end=30")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setItems(result);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        )
+        console.log("hieutt")
+    }, [])
+  
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <>
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              <p>{item.title}</p>
+              <img src={item.url} />
+            </li>
+          ))}
+        </ul> 
+        {props.anhTuyen}
+        </>
+      );
+    }
+}
+
+export default TestImage;
